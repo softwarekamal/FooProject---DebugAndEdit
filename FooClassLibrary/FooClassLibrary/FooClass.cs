d38901f1c55f2886453f035f8c79774c51b85992
Test <Test@TEST-PC>
2022-02-22 14:41:43 +0200

    Add project files.

---
+++ FooClassLibrary/FooClassLibrary/FooClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FooClassLibrary
+{
+    public class FooClass
+    {
+        public string GetData()
+        {
+            string data = "Hello";
+            data = "ok man";
+            return data;
+            
+        }
+    }
+}
---
+++ FooClassLibrary/FooClassLibrary/Properties/VersionInfo.cs
@@ -0,0 +1,1 @@
+[assembly:System.Reflection.AssemblyVersion("2022.2N.20.0251")]
